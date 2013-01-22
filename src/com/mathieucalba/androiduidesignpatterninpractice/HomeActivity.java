b231b8acafbe4194b7adc774aa2becaa04ff86a6
Mathieu Calba <mathieu.calba@gmail.com>
2013-01-22 22:08:14 +0100

    Creating base project
    - Manifest
    - Launcher icon
    - support library
    - HomeActivity

---
+++ src/com/mathieucalba/androiduidesignpatterninpractice/HomeActivity.java
@@ -0,0 +1,23 @@
+package com.mathieucalba.androiduidesignpatterninpractice;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+
+public class HomeActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_home, menu);
+		return true;
+	}
+
+}
