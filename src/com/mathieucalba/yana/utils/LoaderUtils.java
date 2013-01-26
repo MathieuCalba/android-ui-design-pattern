3b6cba8f0907295148e8566abfdd636c2634a023
Mathieu Calba <mathieu.calba@gmail.com>
2013-01-26 20:36:33 +0100

    fix calls from any fragment or any fragmentactivity

--- src/com/mathieucalba/yana/utils/LoaderUtils.java
+++ src/com/mathieucalba/yana/utils/LoaderUtils.java
@@ -2,15 +2,14 @@ package com.mathieucalba.yana.utils;
 
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.app.LoaderManager.LoaderCallbacks;
 
-import com.actionbarsherlock.app.SherlockFragment;
-import com.actionbarsherlock.app.SherlockFragmentActivity;
-
 public class LoaderUtils {
 
-	public static void restartLoader(SherlockFragmentActivity activity, int id, Bundle args, LoaderCallbacks<Cursor> listener) {
+	public static void restartLoader(FragmentActivity activity, int id, Bundle args, LoaderCallbacks<Cursor> listener) {
 		if (!activity.isFinishing()) {
 			final LoaderManager loaderManager = activity.getSupportLoaderManager();
 			if (loaderManager != null) {
@@ -19,7 +18,7 @@ public class LoaderUtils {
 		}
 	}
 
-	public static void restartLoader(SherlockFragment fragment, int id, Bundle args, LoaderCallbacks<Cursor> listener) {
+	public static void restartLoader(Fragment fragment, int id, Bundle args, LoaderCallbacks<Cursor> listener) {
 		if (fragment.getActivity() != null && !fragment.isDetached()) {
 			final LoaderManager loaderManager = fragment.getActivity().getSupportLoaderManager();
 			if (loaderManager != null) {
