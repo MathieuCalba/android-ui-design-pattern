95ebce071368e9bff5fd7063c7e144d3e30833ae
Mathieu Calba <mathieu.calba@gmail.com>
2013-02-10 23:24:00 +0100

    removing item screen managing

--- src/com/mathieucalba/yana/ui/fragments/FeedListFragment.java
+++ src/com/mathieucalba/yana/ui/fragments/FeedListFragment.java
@@ -1,6 +1,5 @@
 package com.mathieucalba.yana.ui.fragments;
 
-import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -11,20 +10,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
 import com.actionbarsherlock.app.SherlockFragment;
 import com.mathieucalba.yana.R;
-import com.mathieucalba.yana.model.FeedsData;
 import com.mathieucalba.yana.provider.YANAContract;
-import com.mathieucalba.yana.ui.activity.FeedItemActivity;
 import com.mathieucalba.yana.ui.adapters.FeedListAdapter;
 import com.mathieucalba.yana.utils.LoaderUtils;
 
 
-public class FeedListFragment extends SherlockFragment implements LoaderCallbacks<Cursor>, OnItemClickListener {
+public class FeedListFragment extends SherlockFragment implements LoaderCallbacks<Cursor> {
 
 	public static final String EXTRA_FEED_ID = "com.mathieucalba.yana.EXTRA_FEED_ID";
 	public static final String EXTRA_CATEGORY_ID = "com.mathieucalba.yana.EXTRA_CATEGORY_ID";
@@ -75,7 +70,6 @@ public class FeedListFragment extends SherlockFragment implements LoaderCallback
 		final View v = inflater.inflate(R.layout.fragment_feed_list, container, false);
 
 		mListView = (ListView) v.findViewById(R.id.list_view);
-		mListView.setOnItemClickListener(this);
 		mListView.setChoiceMode(AbsListView.CHOICE_MODE_SINGLE);
 
 		return v;
@@ -161,25 +155,6 @@ public class FeedListFragment extends SherlockFragment implements LoaderCallback
 		final int realId = id - mFeedId * 100 - mCategoryId;
 		if (realId == LOADER_ID_BASE_FEED_LIST) {
 			mFeedListAdapter.swapCursor(null);
-		}
-	}
-
-	@Override
-	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-		// Briefs don't have content details
-		if (mFeedId != FeedsData.FEED_IDS.BRIEF) {
-			final Intent intent = new Intent(getActivity(), FeedItemActivity.class);
-			intent.putExtra(FeedItemActivity.EXTRA_CATEGORY_ID, mCategoryId);
-			intent.putExtra(FeedItemActivity.EXTRA_FEED_ID, mFeedId);
-			final Cursor c = (Cursor) mFeedListAdapter.getItem(position);
-			if (c != null) {
-				final int idItem = c.getInt(YANAContract.ArticleTable.PROJ_LIST.ID);
-				intent.putExtra(FeedItemActivity.EXTRA_ITEM_ID, idItem);
-			}
-			startActivity(intent);
-
-			mListView.setItemChecked(position, true);
-			mCheckedPosition = position;
 		}
 	}
 
